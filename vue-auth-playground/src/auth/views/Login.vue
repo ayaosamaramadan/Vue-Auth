2f1af3fe94011ea57cff11482ac375fb019e783c
ayaosamaramadan <ayaramadan2011@gmail.com>
2026-02-28 23:38:40 +0200

    Google Sign-up

--- vue-auth-playground/src/auth/views/Login.vue
+++ vue-auth-playground/src/auth/views/Login.vue
@@ -1,38 +1,56 @@
+<template>
+  <div class="login">
+    <form @submit.prevent="login">
+      <input type="email" v-model="email" required>
+      <input type="password" v-model="password" required>
+      <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
 
-   <template>
-     <div class="login">
-      <form @submit.prevent="login">
-        <input type="email" v-model="email" required>
-        <input type="password" v-model="password" required>
-        <button type="submit">Login</button>
-      </form>
-      <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
-      <p v-if="error" class="error">{{ error }}</p>
-     </div>
-   </template>
+    <button type="button" @click="googleSignIn">
+      Login with Google
+    </button>
 
-   <script>
-   export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-        error: null
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login() {
+      this.error = null
+      try {
+        await this.$store.dispatch('login', {
+          email: this.email,
+          password: this.password
+        })
+        this.$router.push('/')
+      } catch (err) {
+        this.error = err.code ? `${err.code}: ${err.message}` : err.message || String(err)
       }
     },
-    methods: {
-      async login() {
-        this.error = null
-        try {
-          await this.$store.dispatch('login', {
-            email: this.email,
-            password: this.password
-          })
-             this.$router.push('/')
-        } catch (err) {
-          this.error = err.code ? `${err.code}: ${err.message}` : err.message || String(err)
+    async googleSignIn() {
+      this.error = null
+      try {
+        const { signInWithGoogle } = await import('@/auth/services/googleAuth')
+        const user = await signInWithGoogle()
+        if (user && typeof user.getIdToken === 'function') {
+          const token = await user.getIdToken()
+          this.$store.commit('setToken', token)
         }
+        this.$router.push('/')
+      } catch (err) {
+        this.error = err.code ? `${err.code}: ${err.message}` : err.message || String(err)
       }
     }
-   }
-   </script>
+  }
+}
+</script>
